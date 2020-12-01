ab48a0e924bece5a04d66b78ddcf7b8fa1443c38
Matt Grande <mgrande@reliqhealth.com>
2020-12-01 10:55:45 -0500

    Day 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
@@ -15,11 +16,40 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
+            var lines = Input.Split('\n');
+            var nums = lines.Select(s => Int32.Parse(s)).ToList();
+            for (var i = 0; i < nums.Count(); i++)
+            {
+                for (var j = i; j < nums.Count(); j++)
+                {
+                    var sum = nums[i] + nums[j];
+                    if (sum == 2020)
+                    {
+                        return (nums[i] * nums[j]).ToString();
+                    }
+                }
+            }
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            var lines = Input.Split('\n');
+            var nums = lines.Select(s => Int32.Parse(s)).ToList();
+            for (var i = 0; i < nums.Count(); i++)
+            {
+                for (var j = i; j < nums.Count(); j++)
+                {
+                    for (var k = j; k < nums.Count(); k++)
+                    {
+                        var sum = nums[i] + nums[j] + nums[k];
+                        if (sum == 2020)
+                        {
+                            return (nums[i] * nums[j] * nums[k]).ToString();
+                        }
+                    }
+                }
+            }
             return null;
         }
     }
