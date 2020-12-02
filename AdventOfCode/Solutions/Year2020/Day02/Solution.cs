747c041af9d02dcf50236b7117a806364de871bb
Matt Grande <mgrande@reliqhealth.com>
2020-12-02 13:11:51 -0500

    Day2, pt 1

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,20 +8,39 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day02 : ASolution
     {
-
         public Day02() : base(02, 2020, "")
         {
-
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var lines = Input.Split('\n');
+            var c = lines.Where(s => IsValid(s))
+                         .Count();
+            return c.ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        private bool IsValid(string input)
+        {
+            var split = input.Split(':');
+            var nums = split[0];
+            var pw = split[1].Trim();
+
+            var split2 = nums.Split(' ');
+            var split3 = split2[0].Split('-');
+
+            var lowerBound = int.Parse(split3[0]);
+            var upperBound = int.Parse(split3[1]);
+            var letter = split2[1][0];
+
+            var count = pw.Where(c => c == letter).Count();
+
+            return count >= lowerBound && count <= upperBound;
         }
     }
 }
