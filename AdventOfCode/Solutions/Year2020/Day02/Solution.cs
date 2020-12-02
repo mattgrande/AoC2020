7368b0969c104d66719c0174bcfb9adc58f8c5cf
Matt Grande <mgrande@reliqhealth.com>
2020-12-02 13:16:36 -0500

    Day 2, Pt 2

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -15,17 +15,20 @@ namespace AdventOfCode.Solutions.Year2020
         protected override string SolvePartOne()
         {
             var lines = Input.Split('\n');
-            var c = lines.Where(s => IsValid(s))
+            var c = lines.Where(s => IsValidOne(s))
                          .Count();
             return c.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var lines = Input.Split('\n');
+            var c = lines.Where(s => IsValidTwo(s))
+                         .Count();
+            return c.ToString();
         }
 
-        private bool IsValid(string input)
+        private bool IsValidOne(string input)
         {
             var split = input.Split(':');
             var nums = split[0];
@@ -41,6 +44,22 @@ namespace AdventOfCode.Solutions.Year2020
             var count = pw.Where(c => c == letter).Count();
 
             return count >= lowerBound && count <= upperBound;
+        }
+
+        private bool IsValidTwo(string input)
+        {
+            var split = input.Split(':');
+            var nums = split[0];
+            var pw = split[1].Trim();
+
+            var split2 = nums.Split(' ');
+            var split3 = split2[0].Split('-');
+
+            var index1 = int.Parse(split3[0]) - 1;
+            var index2 = int.Parse(split3[1]) - 1;
+            var letter = split2[1][0];
+
+            return (pw[index1] == letter ^ pw[index2] == letter);
         }
     }
 }
