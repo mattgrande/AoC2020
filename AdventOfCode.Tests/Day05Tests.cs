9f8b5ff61ca0724a59c78d3a6d6d7c2ffb4f6cd3
Matt Grande <mgrande@reliqhealth.com>
2020-12-05 11:03:19 -0500

    Day 5 cleanup

--- AdventOfCode.Tests/Day05Tests.cs
+++ AdventOfCode.Tests/Day05Tests.cs
@@ -9,33 +9,29 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_FindRow()
         {
-            var d5 = new Day05();
-            var row = d5.FindRow("FBFBBFF");
+            var row = Day05.FindRow("FBFBBFF");
             Assert.AreEqual(44, row);
         }
 
         [Test]
         public void Test_FindSeat()
         {
-            var d5 = new Day05();
-            var column = d5.FindSeat("RLR");
+            var column = Day05.FindSeat("RLR");
             Assert.AreEqual(5, column);
         }
 
         [Test]
         public void Test_CutInHalf()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('F', Enumerable.Range(0, 128)).ToList();
+            var range = Day05.CutInHalf('F', Enumerable.Range(0, 128)).ToList();
             Assert.AreEqual(0, range[0]);
-            Assert.AreEqual(63, range[range.Count() - 1]);
+            Assert.AreEqual(63, range[^1]);
         }
 
         [Test]
         public void Test_CutInHalf_2()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('B', Enumerable.Range(0, 64)).ToList();
+            var range = Day05.CutInHalf('B', Enumerable.Range(0, 64)).ToList();
             Assert.AreEqual(32, range[0]);
             Assert.AreEqual(63, range[range.Count() - 1]);
         }
@@ -43,8 +39,7 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_CutInHalf_3()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('F', Enumerable.Range(32, 32)).ToList();
+            var range = Day05.CutInHalf('F', Enumerable.Range(32, 32)).ToList();
             Assert.AreEqual(32, range[0]);
             Assert.AreEqual(47, range[range.Count() - 1]);
         }
@@ -52,8 +47,7 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_CutInHalf_4()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('B', Enumerable.Range(32, 16)).ToList();
+            var range = Day05.CutInHalf('B', Enumerable.Range(32, 16)).ToList();
             Assert.AreEqual(40, range[0]);
             Assert.AreEqual(47, range[range.Count() - 1]);
         }
@@ -61,8 +55,7 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_CutInHalf_5()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('B', Enumerable.Range(40, 8)).ToList();
+            var range = Day05.CutInHalf('B', Enumerable.Range(40, 8)).ToList();
             Assert.AreEqual(44, range[0]);
             Assert.AreEqual(47, range[range.Count() - 1]);
         }
@@ -70,8 +63,7 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_CutInHalf_6()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('F', Enumerable.Range(44, 4)).ToList();
+            var range = Day05.CutInHalf('F', Enumerable.Range(44, 4)).ToList();
             Assert.AreEqual(44, range[0]);
             Assert.AreEqual(45, range[range.Count() - 1]);
         }
@@ -79,19 +71,17 @@ namespace AdventOfCode.Tests
         [Test]
         public void Test_CutInHalf_Last()
         {
-            var d5 = new Day05();
-            var range = d5.CutInHalf('F', Enumerable.Range(44, 2)).ToList();
+            var range = Day05.CutInHalf('F', Enumerable.Range(44, 2)).ToList();
             Assert.AreEqual(44, range[0]);
 
-            var range2 = d5.CutInHalf('B', Enumerable.Range(44, 2)).ToList();
+            var range2 = Day05.CutInHalf('B', Enumerable.Range(44, 2)).ToList();
             Assert.AreEqual(45, range2[0]);
         }
 
         [Test]
         public void Test_FindSeatIndex()
         {
-            var d5 = new Day05();
-            var i = d5.FindSeatIndex("FBFBBFFRLR");
+            var i = Day05.FindSeatIndex("FBFBBFFRLR");
             Assert.AreEqual(357, i);
         }
     }
--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -1,36 +1,26 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
     public class Day05 : ASolution
     {
         public Day05() : base(05, 2020, "")
-        {
-
-        }
+        {}
 
         protected override string SolvePartOne()
         {
             var lines = Input.SplitByNewline();
-            var max = 0;
-            foreach (var line in lines)
-            {
-                var i = FindSeatIndex(line);
-                max = Math.Max(max, i);
-            }
+            var max = lines.Select(FindSeatIndex).Concat(new[] {0}).Max();
             return max.ToString();
         }
 
         protected override string SolvePartTwo()
         {
             var lines = Input.SplitByNewline();
-            var seatIndicies = lines.Select(l => FindSeatIndex(l)).OrderBy(i => i).ToList();
+            var seatIndicies = lines.Select(FindSeatIndex).OrderBy(i => i).ToList();
             var lowestSeat = seatIndicies[0];
-            var biggestSeat = seatIndicies[seatIndicies.Count - 1];
-            var unusedSeats = new List<int>();
+            var biggestSeat = seatIndicies[^1];
             foreach (var i in Enumerable.Range(lowestSeat, biggestSeat - lowestSeat))
             {
                 if (! seatIndicies.Contains(i))
@@ -41,7 +31,7 @@ namespace AdventOfCode.Solutions.Year2020
             return null;
         }
 
-        public int FindSeatIndex(string s)
+        public static int FindSeatIndex(string s)
         {
             var rowPart = s.Substring(0, 7);
             var seatPart = s.Substring(7);
@@ -52,31 +42,28 @@ namespace AdventOfCode.Solutions.Year2020
             return (row * 8) + seat;
         }
 
-        public int FindRow(string s)
+        public static int FindRow(string s)
         {
             return PartitionToNumber(s, Enumerable.Range(0, 128));
         }
 
-        public int FindSeat(string s)
+        public static int FindSeat(string s)
         {
             return PartitionToNumber(s, Enumerable.Range(0, 8));
         }
 
-        public int PartitionToNumber(string s, IEnumerable<int> range)
+        public static int PartitionToNumber(string s, IEnumerable<int> range)
         {
-            for (var i = 0; i < s.Length; i++)
-            {
-                char fOrB = s[i];
-                range = CutInHalf(fOrB, range);
-            }
+            range = s.Aggregate(range, (current, fOrB) => CutInHalf(fOrB, current));
 
             return range.ElementAt(0);
         }
 
-        public IEnumerable<int> CutInHalf(char fOrB, IEnumerable<int> range)
+        public static IEnumerable<int> CutInHalf(char fOrB, IEnumerable<int> range)
         {
-            var midpoint = range.Count() / 2;
-            var ranges = range.Split(midpoint);
+            var arr = range.ToArray();
+            var midpoint = arr.Length / 2;
+            var ranges = arr.Split(midpoint);
             var i = (fOrB == 'F' || fOrB == 'L') ? 0 : 1;
             return ranges.ElementAt(i);
         }
