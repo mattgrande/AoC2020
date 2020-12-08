fe6801be89dd0c8befdcd9429bb6fc1011b0f590
Matt Grande <mgrande@reliqhealth.com>
2020-12-08 10:03:37 -0500

    Day 8 Pt 2

--- AdventOfCode/Solutions/Year2020/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day08/Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -21,13 +22,59 @@ namespace AdventOfCode.Solutions.Year2020
         public int SolvePt1()
         {
             var lines = Input.SplitByNewline();
+            return RunProgram(lines).Item1;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return SolvePt2().ToString();
+        }
+
+        public int SolvePt2()
+        {
+            var lines = Input.SplitByNewline();
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                if (line.StartsWith("acc")) continue;
+
+                var flippedCopy = lines.ToArray();
+                flippedCopy[i] = FlipInstruction(line);
+                var result = RunProgram(flippedCopy);
+                if (result.Item2 == false)
+                {
+                    return result.Item1;
+                }
+            }
+
+            return 0;
+        }
+
+        private static Instruction ParseInstruction(string input)
+        {
+            var parts = input.Split(' ');
+            return new Instruction
+            {
+                Command = parts[0],
+                Value = int.Parse(parts[1]),
+            };
+        }
+
+        private static Tuple<int, bool> RunProgram(string[] lines)
+        {
+            var infiniteLoop = false;
             var acc = 0;
             var visitedCommands = new SortedSet<int>();
 
             for (var i = 0; i < lines.Length;)
             {
                 Console.WriteLine("{0}: {1} ({2})", i, lines[i], acc);
-                if (visitedCommands.Contains(i)) break;
+                if (visitedCommands.Contains(i)) 
+                {
+                    infiniteLoop = true;
+                    break;
+                }
 
                 visitedCommands.Add(i);
                 var inst = ParseInstruction(lines[i]);
@@ -49,27 +96,14 @@ namespace AdventOfCode.Solutions.Year2020
                 }
             }
 
-            return acc;
+            Console.WriteLine("Done! {0},{1}", acc, infiniteLoop);
+            return new Tuple<int, bool>(acc, infiniteLoop);
         }
 
-        protected override string SolvePartTwo()
+        private static string FlipInstruction(string inst)
         {
-            return SolvePt2().ToString();
-        }
-
-        public int SolvePt2()
-        {
-            return 0;
-        }
-
-        public Instruction ParseInstruction(string input)
-        {
-            var parts = input.Split(' ');
-            return new Instruction
-            {
-                Command = parts[0],
-                Value = int.Parse(parts[1]),
-            };
+            var parts = inst.Split(' ');
+            return parts[0] == "jmp" ? $"nop {parts[1]}" : $"jmp {parts[1]}";
         }
 
         public class Instruction
