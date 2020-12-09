0a323ee2e34d0ec4b488c1ff502284220794b4d1
Matt Grande <mgrande@reliqhealth.com>
2020-12-09 12:13:04 -0500

    Day 9 pt 1

---
+++ AdventOfCode.Tests/Day09Tests.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using AdventOfCode.Solutions.Year2020;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day09Tests
+    {
+        [Test]
+        public void Test_SplitPreamble()
+        {
+            var d9 = new Day09();
+            d9.DebugInput = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+            var parts = d9.SplitPreamble(5);
+            Assert.AreEqual(5, parts[0].Count);
+            Assert.AreEqual(15, parts[1].Count);
+            Assert.AreEqual(40, parts[1][0]);
+        }
+
+        [Test]
+        public void Test_PossibleSums()
+        {
+            var l = new List<int> { 5, 2, 3, 4, 1 };
+            var sums = Day09.GetPossibleSums(l);
+            Assert.AreEqual(7, sums.Count());
+            Assert.AreEqual(3, sums[0]);
+            Assert.AreEqual(9, sums[^1]);
+        }
+
+        [Test]
+        public void Test_SolvePt1()
+        {
+            var d9 = new Day09();
+            d9.DebugInput = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+            var result = d9.SolvePt1(5);
+            Assert.AreEqual(127, result);
+        }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day09/Solution.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day09 : ASolution
+    public class Day09 : ASolution
     {
 
         public Day09() : base(09, 2020, "")
@@ -15,12 +15,51 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+            return SolvePt1().ToString();
+        }
+
+        public int SolvePt1(int preambleSize = 25)
+        {
+            var parts = SplitPreamble(preambleSize);
+            var preamble = parts[0];
+            var numbers = parts[1];
+
+            foreach (var n in numbers)
+            {
+                var sums = GetPossibleSums(preamble);
+                if (! sums.Contains(n)) return n;
+
+                preamble.RemoveAt(0);
+                preamble.Add(n);
+            }
+
+            return 0;
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        public IList<IList<int>> SplitPreamble(int preambleSize = 25)
+        {
+            var ints = Input.ToIntArray("\n").ToList();
+            var preamble = ints.Take(preambleSize).ToList();
+            var theRest = ints.Skip(preambleSize).ToList();
+            return new List<IList<int>> { preamble, theRest };
+        }
+
+        public static IList<int> GetPossibleSums(IList<int> preamble)
+        {
+            var sums = new List<int>();
+            for (int i = 0; i < preamble.Count; i++)
+            {
+                for (int j = (i + 1); j < preamble.Count; j++)
+                {
+                    sums.Add(preamble[i] + preamble[j]);
+                }
+            }
+            return sums.OrderBy(i => i).Distinct().ToList();
         }
     }
 }
