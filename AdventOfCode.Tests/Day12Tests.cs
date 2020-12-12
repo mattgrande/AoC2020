603b08fff388a5774ae4e6a0ab54570b416eb2a5
Matt Grande <mgrande@reliqhealth.com>
2020-12-12 10:34:23 -0500

    Day 12, Pt 1

---
+++ AdventOfCode.Tests/Day12Tests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using AdventOfCode.Solutions.Year2020;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day12Tests
+    {
+        [Test]
+        public void Test_MoveF()
+        {
+            var d12 = new Day12();
+            d12.Move("F10");
+            Assert.AreEqual(d12.NS, 0);
+            Assert.AreEqual(d12.EW, 10);
+        }
+        
+        [Test]
+        public void Test_MoveN()
+        {
+            var d12 = new Day12('E', 0, 10);
+            d12.Move("N3");
+            Assert.AreEqual(d12.Facing, 'E');
+            Assert.AreEqual(d12.NS, 3);
+            Assert.AreEqual(d12.EW, 10);
+        }
+        
+        [Test]
+        public void Test_MoveR()
+        {
+            var d12 = new Day12('E', 3, 10);
+            d12.Move("R90");
+            Assert.AreEqual(d12.Facing, 'S');
+            Assert.AreEqual(d12.NS, 3);
+            Assert.AreEqual(d12.EW, 10);
+
+            d12.Move("R180");
+            Assert.AreEqual(d12.Facing, 'N');
+            d12.Move("R270");
+            Assert.AreEqual(d12.Facing, 'W');
+        }
+
+        [Test]
+        public void Test_GetManhattanDistance()
+        {
+            var d12 = new Day12();
+            d12.DebugInput = @"F10
+N3
+F7
+R90
+F11";
+            var mDist = d12.CalculateManhattanDistance();
+            Assert.AreEqual(25, mDist);
+        }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day12/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day12/Solution.cs
@@ -1,26 +1,89 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
-    class Day12 : ASolution
+    public class Day12 : ASolution
     {
-
-        public Day12() : base(12, 2020, "")
+        public char Facing { get; private set; }
+        public int NS { get; private set; }
+        public int EW { get; private set; }
+        public Day12() : this('E', 0, 0) {}
+        public Day12(char facing = 'E', int ns = 0, int ew = 0) : base(12, 2020, "")
         {
-
+            Facing = facing;
+            NS = ns;
+            EW = ew;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return CalculateManhattanDistance().ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        public int CalculateManhattanDistance()
+        {
+            var instructions = Input.SplitByNewline();
+            foreach (var inst in instructions)
+            {
+                Move(inst);
+            }
+            return Math.Abs(NS) + Math.Abs(EW);
+        }
+
+        public void Move(string inst)
+        {
+            var command = inst[0];
+            var n = int.Parse(inst.Substring(1));
+            switch (command)
+            {
+                case 'F':
+                    var mult = (Facing == 'W' || Facing == 'S') ? -1 : 1;
+                    n *= mult;
+
+                    if (Facing == 'E' || Facing == 'W') EW += n;
+                    if (Facing == 'N' || Facing == 'S') NS += n;
+                    break;
+                case 'N':
+                    NS += n;
+                    break;
+                case 'S':
+                    NS -= n;
+                    break;
+                case 'E':
+                    EW += n;
+                    break;
+                case 'W':
+                    EW -= n;
+                    break;
+                case 'R':
+                    {
+                    var rotations = n / 90;
+                    var directions = new [] { 'E', 'S', 'W', 'N' }.ToList();
+                    var currentIndex = directions.FindIndex(f => f == Facing);
+                    Facing = directions[(currentIndex + rotations) % 4];
+                    }
+                    break;
+                case 'L':
+                    {
+                    var rotations = n / 90;
+                    var directions = new [] { 'E', 'S', 'W', 'N' }.ToList();
+                    var currentIndex = directions.FindIndex(f => f == Facing);
+                    var newIndex = currentIndex - rotations;
+                    // Console.WriteLine("NI:{0}", newIndex);
+                    Facing = newIndex >= 0 ? directions[newIndex % 4] : directions[^Math.Abs(newIndex)];
+                    }
+                    break;
+            }
+            return;
         }
     }
 }
