d8767972eae657eb9a35430909372d3584023809
Matt Grande <mgrande@reliqhealth.com>
2020-12-13 09:50:42 -0500

    Day 13 Pt 1

--- AdventOfCode/Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day13/Solution.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
-    class Day13 : ASolution
+    public class Day13 : ASolution
     {
-
         public Day13() : base(13, 2020, "")
         {
 
@@ -15,7 +15,30 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+//             DebugInput = @"939
+// 7,13,x,x,59,x,31,19";
+
+            var lines = Input.Split('\n');
+            var arrival = int.Parse(lines[0]);
+            var buses = lines[1].Split(',')
+                                .Where(b => b != "x")
+                                .Select(b => int.Parse(b));
+
+            var min = int.MaxValue;
+            var result = 0;
+            foreach (var b in buses)
+            {
+                var x = (arrival / b) + 1;
+                var m = (x * b) - arrival;
+                if (m < min)
+                {
+                    min = m;
+                    result = m * b;
+                }
+                Console.WriteLine("Bus {0} will depart in {1} minutes", b, m);
+            }
+
+            return result.ToString();
         }
 
         protected override string SolvePartTwo()
